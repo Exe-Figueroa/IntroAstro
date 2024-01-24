f7b9b88e099a5d75643dba6c1dcc214560828bab
FacundoExequielFigueroa <facundofigueroa789@gmail.com>
2024-01-24 11:36:23 -0300

    COSO

---
+++ Notitas.js
@@ -0,0 +1,11 @@
+/**
+ * *La parte dentro de los 3 - es donde va el js
+ * *Los estilos tienen scope
+ * *En ./layouts se colocan los componentes que envuelven a otros componentes 
+ * *El layout tiene una interface para definir el tipo de datos que queremos oobtener
+ * *Para obtener el dato o propiedad simplemente hacemos un destructuring con el nombre de la propiedad de Astro.props
+ *  ?Astro => variable global que funciona en todos los archivos .astro y que contiene diferente info
+ * *Los estilos también pueden ser globaless con is:global
+ * *Astro no tiene boilerplate
+ * *npm astro add --help => Lista cosas para añadir al proyecto 
+ */  
